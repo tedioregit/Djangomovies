5a5bbf938b7ebdb17b3406add7ecc55d6f328b12
tedioregit <molchanovskyi.oleh@gmail.com>
2023-03-18 08:02:25 -0400

    Individual page for each movie. URL reference

--- movies/models.py
+++ movies/models.py
@@ -16,4 +16,3 @@ class Movie(models.Model):
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
-#COMENT 1
